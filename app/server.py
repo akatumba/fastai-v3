80238ff9101c875a60dffee6300eb6bc47d31f8f
Andrew Katumba <andrew.katumba@ugent.be>
2019-02-05 22:36:03 +0000

    Change to load learner cpu

--- app/server.py
+++ app/server.py
@@ -10,7 +10,7 @@ from fastai import *
 from fastai.vision import *
 from PIL import Image
 import torch
-defaults.device = torch.device('cpu')
+# defaults.device = torch.device('cpu')
 export_file_url = 'https://www.dropbox.com/s/qn2f12rjwicu8p4/more_iters.pkl?raw=1'
 export_file_name = 'export.pkl'
 
@@ -230,7 +230,7 @@ async def download_file(url, dest):
 async def setup_learner():
     await download_file(export_file_url, path/export_file_name)
     try:
-        learn = load_learner(path, export_file_name)
+        learn = load_learner_cpu(path, export_file_name)
         return learn
     except RuntimeError as e:
         if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
